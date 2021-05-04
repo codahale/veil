dc67f4720d0cd9c953c849d505d351da2d036f0a
Coda Hale <coda.hale@gmail.com>
2021-05-04 09:06:59 -0600

    Add benchmarks for signing.

--- veil-lib/benches/veil_bench.rs
+++ veil-lib/benches/veil_bench.rs
@@ -4,14 +4,14 @@ use criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion};
 
 use veil_lib::SecretKey;
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("encrypt");
-    group.bench_with_input("0KB", &0, bench_encrypt);
-    group.bench_with_input("1KB", &1_000, bench_encrypt);
-    group.bench_with_input("10KB", &10_000, bench_encrypt);
-    group.bench_with_input("100KB", &100_000, bench_encrypt);
-    group.bench_with_input("1MB", &1_000_000, bench_encrypt);
-    group.bench_with_input("10MB", &10_000_000, bench_encrypt);
+fn criterion_encrypt(c: &mut Criterion) {
+    let mut encrypt = c.benchmark_group("encrypt");
+    encrypt.bench_with_input("0KB", &0, bench_encrypt);
+    encrypt.bench_with_input("1KB", &1_000, bench_encrypt);
+    encrypt.bench_with_input("10KB", &10_000, bench_encrypt);
+    encrypt.bench_with_input("100KB", &100_000, bench_encrypt);
+    encrypt.bench_with_input("1MB", &1_000_000, bench_encrypt);
+    encrypt.bench_with_input("10MB", &10_000_000, bench_encrypt);
 }
 
 fn bench_encrypt(b: &mut Bencher, n: &u64) {
@@ -33,7 +33,24 @@ fn bench_encrypt(b: &mut Bencher, n: &u64) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn criterion_sign(c: &mut Criterion) {
+    let mut sign = c.benchmark_group("sign");
+    sign.bench_with_input("0KB", &0, bench_sign);
+    sign.bench_with_input("1KB", &1000, bench_sign);
+    sign.bench_with_input("10KB", &10_000, bench_sign);
+    sign.bench_with_input("100KB", &100_000, bench_sign);
+    sign.bench_with_input("1MB", &1_000_000, bench_sign);
+    sign.bench_with_input("10MB", &10_000_000, bench_sign);
+}
+
+fn bench_sign(b: &mut Bencher, n: &u64) {
+    let sk_a = SecretKey::new();
+    let pk_a = sk_a.private_key("/one/two");
+
+    b.iter(|| pk_a.sign(&mut Zero(*n)).unwrap());
+}
+
+criterion_group!(benches, criterion_encrypt, criterion_sign);
 criterion_main!(benches);
 
 struct Zero(u64);
