e845c362d7cf4f09bb8b99a2a898f311e29f8c80
Coda Hale <coda.hale@gmail.com>
2021-05-09 09:48:37 -0600

    Use duct instead of assert_cmd.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,10 +1,11 @@
 #![cfg(feature = "cli")]
-use std::fs;
+
 use std::path::PathBuf;
 use std::str;
+use std::{env, fs};
 
 use anyhow::Result;
-use assert_cmd::Command;
+use duct::cmd;
 
 #[test]
 pub fn bootstrap_and_send_a_message() -> Result<()> {
@@ -38,35 +39,37 @@ pub fn bootstrap_and_send_a_message() -> Result<()> {
 
     // Alice encrypts the message for Bea.
     let ciphertext_path = &dir.path().join("message.veil");
-    let mut cmd = Command::cargo_bin("veil")?;
-    cmd.arg("encrypt")
-        .arg(secret_key_path_a)
-        .arg("/friends/bea")
-        .arg(message_file)
-        .arg(ciphertext_path)
-        .arg(&public_key_b)
-        .arg("--fakes")
-        .arg("20")
-        .arg("--padding")
-        .arg("1204")
-        .arg("--passphrase-file")
-        .arg(passphrase_path_a)
-        .assert()
-        .success();
+    cmd!(
+        bin_path(),
+        "encrypt",
+        secret_key_path_a,
+        "/friends/bea",
+        message_file,
+        ciphertext_path,
+        &public_key_b,
+        "--fakes",
+        "20",
+        "--padding",
+        "1024",
+        "--passphrase-file",
+        passphrase_path_a,
+    )
+    .run()?;
 
     // Bea decrypts the message.
     let plaintext_path = &dir.path().join("message.txt");
-    let mut cmd = Command::cargo_bin("veil")?;
-    cmd.arg("decrypt")
-        .arg(secret_key_path_b)
-        .arg("/friends/alice")
-        .arg(ciphertext_path)
-        .arg(plaintext_path)
-        .arg(&public_key_a)
-        .arg("--passphrase-file")
-        .arg(passphrase_path_b)
-        .assert()
-        .success();
+    cmd!(
+        bin_path(),
+        "decrypt",
+        secret_key_path_b,
+        "/friends/alice",
+        ciphertext_path,
+        plaintext_path,
+        &public_key_a,
+        "--passphrase-file",
+        passphrase_path_b,
+    )
+    .run()?;
 
     // Bea reads the message.
     let msg = fs::read_to_string(plaintext_path)?;
@@ -80,32 +83,48 @@ fn generate_public_key(
     passphrase_path: &PathBuf,
     key_id: &str,
 ) -> Result<String> {
-    let mut cmd = Command::cargo_bin("veil")?;
-    let success = cmd
-        .arg("public-key")
-        .arg(secret_key_path)
-        .arg(key_id)
-        .arg("--passphrase-file")
-        .arg(passphrase_path)
-        .assert()
-        .success();
-    let out = success.get_output();
-    let public_key = str::from_utf8(&out.stdout)?.trim();
-    Ok(public_key.to_string())
+    let out = cmd!(
+        bin_path(),
+        "public-key",
+        secret_key_path,
+        key_id,
+        "--passphrase-file",
+        passphrase_path
+    )
+    .read()?;
+    Ok(out)
 }
 
 fn create_secret_key(secret_key_path: &PathBuf, passphrase_path: &PathBuf) -> Result<()> {
-    let mut cmd = Command::cargo_bin("veil")?;
-    cmd.arg("secret-key")
-        .arg(secret_key_path)
-        .arg("--passphrase-file")
-        .arg(passphrase_path)
-        .arg("--time")
-        .arg("10")
-        .arg("--space")
-        .arg("15")
-        .assert()
-        .success();
+    cmd!(
+        bin_path(),
+        "secret-key",
+        secret_key_path,
+        "--passphrase-file",
+        passphrase_path,
+        "--time",
+        "10",
+        "--space",
+        "15",
+    )
+    .stdout_capture()
+    .run()?;
 
     Ok(())
+}
+
+fn bin_path() -> PathBuf {
+    // Adapted from
+    // https://github.com/rust-lang/cargo/blob/485670b3983b52289a2f353d589c57fae2f60f82/tests/testsuite/support/mod.rs#L507
+    let target_dir = env::current_exe()
+        .ok()
+        .map(|mut path| {
+            path.pop();
+            if path.ends_with("deps") {
+                path.pop();
+            }
+            path
+        })
+        .unwrap();
+    target_dir.join(format!("veil{}", env::consts::EXE_SUFFIX))
 }
