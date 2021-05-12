a4a8af5768374ed1125b410fb6ba64f03554ec2f
Coda Hale <coda.hale@gmail.com>
2021-05-12 16:00:16 -0600

    Include shell completion in git.

--- veil-cli/build.rs
+++ veil-cli/build.rs
@@ -3,11 +3,14 @@ use std::env;
 use clap::IntoApp;
 use clap_generate::generate_to;
 use clap_generate::generators::{Bash, Fish, PowerShell, Zsh};
+use std::path::Path;
 
 include!("src/cli.rs");
 
 fn main() {
-    if let Some(out_dir) = env::var_os("OUT_DIR") {
+    if let Some(out_dir) = env::var_os("CARGO_MANIFEST_DIR") {
+        let out_dir = Path::new(&out_dir).join("share");
+
         let mut app = Opts::into_app();
         let app_name = "veil";
         generate_to::<Bash, _, _>(&mut app, app_name, &out_dir);
