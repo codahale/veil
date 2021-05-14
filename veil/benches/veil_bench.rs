a3c3f081c84070b8ea5ef5cb6d91d46a69091414
Coda Hale <coda.hale@gmail.com>
2021-05-14 13:15:43 -0600

    Add benchmarks for decryption and verification.

--- veil/benches/veil_bench.rs
+++ veil/benches/veil_bench.rs
@@ -1,5 +1,5 @@
 use std::io;
-use std::io::Read;
+use std::io::{Cursor, Read};
 
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 
@@ -13,12 +13,12 @@ fn bench_encrypt(c: &mut Criterion) {
     let pk_b = sk_b.private_key("/three/four");
 
     let mut encrypt = c.benchmark_group("encrypt");
-    for i in vec![0 * KB, 1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB] {
-        encrypt.throughput(Throughput::Elements(i));
-        encrypt.bench_with_input(BenchmarkId::from_parameter(i), &i, |b, n| {
+    for n in vec![0 * KB, 1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB] {
+        encrypt.throughput(Throughput::Elements(n));
+        encrypt.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
             b.iter(|| {
                 pk_a.encrypt(
-                    &mut io::repeat(0).take(*n),
+                    &mut io::repeat(0).take(n),
                     &mut io::sink(),
                     vec![pk_b.public_key()],
                     black_box(0),
@@ -31,18 +31,68 @@ fn bench_encrypt(c: &mut Criterion) {
     encrypt.finish();
 }
 
+fn bench_decrypt(c: &mut Criterion) {
+    let sk_a = SecretKey::new();
+    let pk_a = sk_a.private_key("/one/two");
+
+    let sk_b = SecretKey::new();
+    let pk_b = sk_b.private_key("/three/four");
+
+    let mut decrypt = c.benchmark_group("decrypt");
+    for n in vec![0 * KB, 1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB] {
+        let mut ciphertext = Cursor::new(Vec::new());
+        pk_a.encrypt(
+            &mut io::repeat(0).take(n),
+            &mut ciphertext,
+            vec![pk_b.public_key()],
+            black_box(0),
+            black_box(0),
+        )
+        .unwrap();
+        let ciphertext = ciphertext.into_inner();
+
+        decrypt.throughput(Throughput::Elements(n));
+        decrypt.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, _| {
+            b.iter(|| {
+                pk_b.decrypt(
+                    &mut Cursor::new(ciphertext.clone()),
+                    &mut io::sink(),
+                    &pk_a.public_key(),
+                )
+                .unwrap();
+            });
+        });
+    }
+    decrypt.finish();
+}
+
 fn bench_sign(c: &mut Criterion) {
     let sk_a = SecretKey::new();
     let pk_a = sk_a.private_key("/one/two");
 
     let mut sign = c.benchmark_group("sign");
-    for i in vec![0 * KB, 1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB] {
-        sign.throughput(Throughput::Elements(i));
-        sign.bench_with_input(BenchmarkId::from_parameter(i), &i, |b, n| {
-            b.iter(|| pk_a.sign(&mut io::repeat(0).take(*n)).unwrap());
+    for n in vec![0 * KB, 1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB] {
+        sign.throughput(Throughput::Elements(n));
+        sign.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
+            b.iter(|| pk_a.sign(&mut io::repeat(0).take(n)).unwrap());
         });
     }
     sign.finish();
+}
+
+fn bench_verify(c: &mut Criterion) {
+    let sk_a = SecretKey::new();
+    let pk_a = sk_a.private_key("/one/two");
+
+    let mut verify = c.benchmark_group("verify");
+    for n in vec![0 * KB, 1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB] {
+        let sig = pk_a.sign(&mut io::repeat(0).take(n)).unwrap();
+        verify.throughput(Throughput::Elements(n));
+        verify.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
+            b.iter(|| pk_a.public_key().verify(&mut io::repeat(0).take(n), &sig).unwrap());
+        });
+    }
+    verify.finish();
 }
 
 fn bench_pbenc(c: &mut Criterion) {
@@ -55,5 +105,5 @@ fn bench_pbenc(c: &mut Criterion) {
 
 const KB: u64 = 1024;
 
-criterion_group!(benches, bench_encrypt, bench_sign, bench_pbenc);
+criterion_group!(benches, bench_encrypt, bench_decrypt, bench_sign, bench_verify, bench_pbenc);
 criterion_main!(benches);
