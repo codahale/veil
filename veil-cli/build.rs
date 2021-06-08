6a5ef15ebfa98bcb0b08360ff99ec6c7d9bb864d
Coda Hale <coda.hale@gmail.com>
2021-06-08 16:09:43 -0600

    No need to keep shell completion files around.

--- veil-cli/build.rs
+++ veil-cli/build.rs
@@ -3,14 +3,11 @@ use std::env;
 use clap::IntoApp;
 use clap_generate::generate_to;
 use clap_generate::generators::{Bash, Fish, PowerShell, Zsh};
-use std::path::Path;
 
 include!("src/cli.rs");
 
 fn main() {
-    if let Some(out_dir) = env::var_os("CARGO_MANIFEST_DIR") {
-        let out_dir = Path::new(&out_dir).join("share");
-
+    if let Some(out_dir) = env::var_os("CARGO_BUILD_TARGET_DIR") {
         let mut app = Opts::into_app();
         let app_name = "veil";
         generate_to::<Bash, _, _>(&mut app, app_name, &out_dir);
