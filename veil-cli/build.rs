0f2afca61687d667ddeeb377da50effc0bd31780
Coda Hale <coda.hale@gmail.com>
2021-12-11 08:16:14 -0700

    Simplify build script error handling.

--- veil-cli/build.rs
+++ veil-cli/build.rs
@@ -1,18 +1,20 @@
 use std::env;
 
-use clap::IntoApp;
+use clap::{Error, IntoApp};
 use clap_generate::generate_to;
 use clap_generate::generators::{Bash, Fish, PowerShell, Zsh};
 
 include!("src/cli.rs");
 
-fn main() {
+fn main() -> Result<(), Error> {
     if let Some(out_dir) = env::var_os("CARGO_BUILD_TARGET_DIR") {
         let mut app = Opts::into_app();
         let app_name = "veil";
-        generate_to(Bash, &mut app, app_name, &out_dir).expect("bash");
-        generate_to(Fish, &mut app, app_name, &out_dir).expect("fish");
-        generate_to(PowerShell, &mut app, app_name, &out_dir).expect("powershell");
-        generate_to(Zsh, &mut app, app_name, &out_dir).expect("zsh");
+        generate_to(Bash, &mut app, app_name, &out_dir)?;
+        generate_to(Fish, &mut app, app_name, &out_dir)?;
+        generate_to(PowerShell, &mut app, app_name, &out_dir)?;
+        generate_to(Zsh, &mut app, app_name, &out_dir)?;
     }
+
+    Ok(())
 }
