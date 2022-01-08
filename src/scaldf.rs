105fae4930cf8151d010ed2b376e0f6e4cd2e9cf
Coda Hale <coda.hale@gmail.com>
2022-01-08 13:30:10 -0700

    refactor(scaldf): inline key splitting
    Also, add some tests for actual scalar and point derivation.

--- src/scaldf.rs
+++ src/scaldf.rs
@@ -13,7 +13,7 @@ pub fn derive_root(r: &[u8; 64]) -> Scalar {
 
 /// Derive a scalar from another scalar using the given key ID.
 pub fn derive_scalar(d: Scalar, key_id: &str) -> Scalar {
-    key_id_parts(key_id).iter().fold(d, |d_p, &label| {
+    key_id.trim_matches('/').split('/').fold(d, |d_p, label| {
         let mut label_df = Strobe::new(b"veil.scaldf.label", SecParam::B128);
         label_df.key(label.as_bytes(), false);
         d_p + label_df.prf_scalar()
@@ -25,17 +25,33 @@ pub fn derive_point(q: &RistrettoPoint, key_id: &str) -> RistrettoPoint {
     q + (G * &derive_scalar(Scalar::zero(), key_id))
 }
 
-fn key_id_parts(key_id: &str) -> Vec<&str> {
-    key_id.trim_matches('/').split('/').collect()
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    pub fn key_id_splitting() {
-        assert_eq!(vec!["one", "two", "three"], key_id_parts("/one/two/three"));
-        assert_eq!(vec!["two", "three"], key_id_parts("two/three"));
+    fn scalar_derivation() {
+        let d = Scalar::random(&mut rand::thread_rng());
+        let d1 = derive_scalar(d, "/one");
+        let d2 = derive_scalar(d1, "/two");
+        let d3 = derive_scalar(d2, "/three");
+
+        let d3_p = derive_scalar(d, "/one/two/three");
+
+        assert_eq!(d3_p, d3);
+    }
+
+    #[test]
+    fn point_derivation() {
+        let d = Scalar::random(&mut rand::thread_rng());
+        let q = G * &d;
+
+        let q1 = derive_point(&q, "/one");
+        let q2 = derive_point(&q1, "/two");
+        let q3 = derive_point(&q2, "/three");
+
+        let q3_p = derive_point(&q, "/one/two/three");
+
+        assert_eq!(q3_p, q3);
     }
 }
