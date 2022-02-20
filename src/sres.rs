d6e820dd71d946ae82d998ea79cf2902e6c89047
Coda Hale <coda.hale@gmail.com>
2022-02-20 14:15:22 -0700

    refactor(sres): simplify proof scalar calculation

--- src/sres.rs
+++ src/sres.rs
@@ -48,13 +48,15 @@ pub fn encrypt(d_s: &Scalar, q_s: &Point, q_r: &Point, plaintext: &[u8]) -> Vec<
 
     // Squeeze a challenge scalar.
     let r = sres.squeeze_scalar();
-
-    // Calculate the proof scalar. If the proof scalar is undefined, try again with a different
-    // random commitment scalar.
-    let s = x * (r + d_s).invert();
-    if s == Scalar::zero() {
+    if &-r == d_s {
+        // If we magically happen to extract a challenge scalar which is the same as the sender's
+        // private key but negative, x/(r+dS) will be undefined. Re-try this operation with a
+        // different random commitment scalar.
         return encrypt(d_s, q_s, q_r, plaintext);
     }
+
+    // Calculate the proof scalar.
+    let s = x * (r + d_s).invert();
 
     // Mask the challenge scalar with the top 4 bits of the mask byte.
     out.extend(mask_scalar(r, mask & 0xF0));
