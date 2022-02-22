5242b43ac99ffa1e6e119ec3e1268dc5f6775e52
Coda Hale <coda.hale@gmail.com>
2022-02-22 14:56:20 -0700

    perf: fix benchmark script

--- benches/benchmark-cli.sh
+++ benches/benchmark-cli.sh
@@ -14,10 +14,10 @@ cp target/release/veil target/release/veil-control
 # create a secret key with minimal KDF expansion
 ./target/release/veil secret-key /tmp/secret-key --passphrase-file=README.md --space=1 --time=1
 
-# benchmark encrypting a 1GiB file for 10 recipients
+# benchmark encrypting a 100MiB file for 10 recipients
 hyperfine --warmup 10 \
-  -n control 'cat /dev/zero | head -n 1073741824 | ./target/release/veil-control encrypt --passphrase-file=README.md /tmp/secret-key /one/two - /dev/null H291qG87hgrGkroZiPkFU64i1LBAk2t61LJvZfxqbV9M --fakes 9' \
-  -n experimental 'cat /dev/zero | head -n 1073741824 | ./target/release/veil-experimental encrypt --passphrase-file=README.md /tmp/secret-key /one/two - /dev/null H291qG87hgrGkroZiPkFU64i1LBAk2t61LJvZfxqbV9M --fakes 9'
+  -n control 'head -c 104857600 /dev/zero | ./target/release/veil-control encrypt --passphrase-file=README.md /tmp/secret-key /one/two - /dev/null H291qG87hgrGkroZiPkFU64i1LBAk2t61LJvZfxqbV9M --fakes 9' \
+  -n experimental 'head -c 104857600 /dev/zero | ./target/release/veil-experimental encrypt --passphrase-file=README.md /tmp/secret-key /one/two - /dev/null H291qG87hgrGkroZiPkFU64i1LBAk2t61LJvZfxqbV9M --fakes 9'
 
 # restore the working set
 git stash pop
