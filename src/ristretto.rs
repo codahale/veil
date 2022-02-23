073074d9dce460c946077fc30ef1fbda3a1086d8
Coda Hale <coda.hale@gmail.com>
2022-02-23 14:40:25 -0700

    refactor(ristretto): use a type alias instead of a re-export
    If for no other reason than it plays more nicely with rustfmt.

--- src/ristretto.rs
+++ src/ristretto.rs
@@ -1,8 +1,9 @@
 //! Utility functions for Ristretto operations.
 
 use curve25519_dalek::ristretto::{CompressedRistretto, RistrettoBasepointTable, RistrettoPoint};
+
 /// A scalar on the Ristretto255 curve.
-pub use curve25519_dalek::scalar::Scalar;
+pub type Scalar = curve25519_dalek::scalar::Scalar;
 
 /// A point on the Ristretto255 curve.
 pub type Point = RistrettoPoint;
