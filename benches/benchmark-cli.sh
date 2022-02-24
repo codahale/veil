6817fdd0f9b64a60434f7159593f7378ef0d781b
Coda Hale <coda.hale@gmail.com>
2022-02-24 13:33:05 -0700

    perf: add cli benchmark for signing

--- benches/benchmark-cli.sh
+++ benches/benchmark-cli.sh
@@ -20,11 +20,25 @@ cp target/release/veil target/release/veil-control
 ./target/release/veil-control secret-key /tmp/secret-key --passphrase-file=README.md --space=1 --time=1
 ./target/release/veil-experiment secret-key /tmp/secret-key --passphrase-file=README.md --space=1 --time=1
 
-# benchmark encrypting a 100MiB file for 10 recipients
-hyperfine --warmup 10 -S /bin/sh \
-  -n control 'head -c 104857600 /dev/zero | ./target/release/veil-control encrypt --passphrase-file=README.md /tmp/secret-key /one/two - /dev/null H291qG87hgrGkroZiPkFU64i1LBAk2t61LJvZfxqbV9M --fakes 9' \
-  -n experimental 'head -c 104857600 /dev/zero | ./target/release/veil-experiment encrypt --passphrase-file=README.md /tmp/secret-key /one/two - /dev/null H291qG87hgrGkroZiPkFU64i1LBAk2t61LJvZfxqbV9M --fakes 9' \
-  ;
+case $1 in
+"encrypt")
+  # benchmark encrypting a 100MiB file for 10 recipients
+  hyperfine --warmup 10 -S /bin/sh \
+    -n control 'head -c 104857600 /dev/zero | ./target/release/veil-control encrypt --passphrase-file=README.md /tmp/secret-key /one/two - /dev/null H291qG87hgrGkroZiPkFU64i1LBAk2t61LJvZfxqbV9M --fakes 9' \
+    -n experimental 'head -c 104857600 /dev/zero | ./target/release/veil-experiment encrypt --passphrase-file=README.md /tmp/secret-key /one/two - /dev/null H291qG87hgrGkroZiPkFU64i1LBAk2t61LJvZfxqbV9M --fakes 9' \
+    ;
+  ;;
+"sign")
+  # benchmark signing a 100MiB file
+  hyperfine --warmup 10 -S /bin/sh \
+    -n control 'head -c 104857600 /dev/zero | ./target/release/veil-control sign --passphrase-file=README.md /tmp/secret-key /one/two - /dev/null' \
+    -n experimental 'head -c 104857600 /dev/zero | ./target/release/veil-experiment sign --passphrase-file=README.md /tmp/secret-key /one/two - /dev/null' \
+    ;
+  ;;
+*)
+  echo "unknown benchmark name"
+  ;;
+esac
 
 # restore the working set
 git stash pop
