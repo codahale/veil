a230fe88128fa2cc89e2e12fd09d9c54215aec46
Coda Hale <coda.hale@gmail.com>
2022-03-23 10:01:26 -0600

    build(docs): build out docs script

--- script/docs.sh
+++ script/docs.sh
@@ -2,5 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-latexmk -cd docs/design.tex -pdf
-latexmk -cd docs/manual.tex -pdf
+case "${1:-}" in
+"clean")
+  latexmk -cd docs/design.tex -C
+  latexmk -cd docs/manual.tex -C
+  ;;
+"view")
+  latexmk -cd docs/"$2".tex -pvc
+  ;;
+*)
+  latexmk -cd docs/design.tex -pdf
+  latexmk -cd docs/manual.tex -pdf
+  ;;
+esac
