edc6973b1c4a97447e987be9f03f13b6c2983546
Coda Hale <coda.hale@gmail.com>
2022-10-26 09:07:20 -0600

    build(deps): bump lockstitch from #1a277abc to #888416b01

--- veil/src/schnorr.rs
+++ veil/src/schnorr.rs
@@ -193,7 +193,7 @@ mod tests {
     fn signature_encoding() {
         let (_, _, _, sig) = setup();
         assert_eq!(
-            "j5SxoKbkSo77bfbGa2boBxJjsj4DJUQmPntGGcPYmCBnxEWnMFXJBB5Fs1N5SMmYxKuBWt6UYwqAfTq2XJS7ziQ",
+            "2mxzUqKYvPrmVe7v1GMPoc7GjnDRkesRsFqqUAVPaXsNhTKBLtQ1JKnjSuTHpq8tfAsLnow7b1K9JFmNfAeSQNfX",
             sig.to_string(),
             "invalid encoded signature"
         );
@@ -202,7 +202,7 @@ mod tests {
     #[test]
     fn signature_decoding() {
         let (_, _, _, sig) = setup();
-        let decoded = "j5SxoKbkSo77bfbGa2boBxJjsj4DJUQmPntGGcPYmCBnxEWnMFXJBB5Fs1N5SMmYxKuBWt6UYwqAfTq2XJS7ziQ".parse::<Signature>();
+        let decoded = "2mxzUqKYvPrmVe7v1GMPoc7GjnDRkesRsFqqUAVPaXsNhTKBLtQ1JKnjSuTHpq8tfAsLnow7b1K9JFmNfAeSQNfX".parse::<Signature>();
         assert_eq!(Ok(sig), decoded, "error parsing signature");
 
         assert_eq!(
