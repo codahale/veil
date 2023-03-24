c08de06fd3bfbce762721c028ef5f38355a2f1d3
Coda Hale <coda.hale@gmail.com>
2023-03-24 17:03:20 -0600

    build(deps): bump lockstitch from 0.8.1 to 0.9.0
    BREAKING CHANGE: Invalidates all previous Veil output.

--- veil/src/schnorr.rs
+++ veil/src/schnorr.rs
@@ -197,7 +197,7 @@ mod tests {
     #[test]
     fn signature_kat() {
         let (_, _, _, sig) = setup();
-        let expected = expect!["4SLbmBpHLVz6q9VMRUFpqgoinzdtcxPMyVpuefwwmcDufL3iYZEkZWs2Db7PLy5UfAa7pB8GLYuvvYs1NfVqZHb3"];
+        let expected = expect!["PpUM4oTCvW9Li5tGsX6ZELQbPxFiNTdYUNNRQcXZN3RPGQwfBCGeAWPJvptRsA1wjyp66GsGHD5d5W3jKRGaD8D"];
         expected.assert_eq(&sig.to_string());
     }
 
--- veil/src/veil.rs
+++ veil/src/veil.rs
@@ -204,7 +204,7 @@ mod tests {
         let rng = ChaChaRng::seed_from_u64(0xDEADBEEF);
         let pk = PrivateKey::random(rng).public_key();
 
-        expect!["Cr3n2YDvVoHtTgCmkxLptLdWLStkofVcYzAkhcCoaWRH"].assert_eq(&pk.to_string());
+        expect!["EE1TMT1RaWJTZFyCrFugxZB5Q3rPghDPiP5AnHQ2UHJ8"].assert_eq(&pk.to_string());
 
         let decoded = pk.to_string().parse::<PublicKey>();
         assert_eq!(Ok(pk), decoded, "error parsing public key");
