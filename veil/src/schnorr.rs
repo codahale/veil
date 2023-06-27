db2a25f4fca2c67eb03fc788114a86bddaa07781
Coda Hale <coda.hale@gmail.com>
2023-06-27 09:59:48 -0600

    build(deps): bump lockstitch from 0.11.3 to 0.12.0

--- veil/src/schnorr.rs
+++ veil/src/schnorr.rs
@@ -197,7 +197,7 @@ mod tests {
     #[test]
     fn signature_kat() {
         let (_, _, _, sig) = setup();
-        let expected = expect!["4duLRpZF1HwXEWRexMj62JfJuBZ7j6J25xjqyZZaKbvTcy1AqxjMvQBwMw9SiNGNYdXmP9XKitE4eYHfFvdnkJ8q"];
+        let expected = expect!["2cUKNuJyKDAXMGQ8hSfNVyBw7T4EHMWKEpVPohqv7Q7mZnGAn6KKqe4Xtj3Qm6K3kLNdog7oKwam7FnDwrBfeNz5"];
         expected.assert_eq(&sig.to_string());
     }
 
